a9bd4777d18fb625f127906a4c7169d950610df1
Suchetha15 <37359656+Suchetha15@users.noreply.github.com>
2018-07-17 12:57:48 +0530

    update with link and nav link

--- expensify/app.js
+++ expensify/app.js
@@ -1,5 +1,5 @@
 //app.js
-Import {BrowserRouter, Route} from ‘react-route-dom’;
+Import {BrowserRouter, Route, Switch, Link, NavLink} from “react-route-dom”;
 const ExpenseDashboardPage = () => (
 	<div>
 	This is ExpenseDashboardPage
@@ -10,44 +10,67 @@ const AddExpensePage = () => (
 	This is AddExpensePage
 </div>
 );
-const EditExpensePage = () => (
+const EditExpensePage = (props) => {
+  console.log(props);
+  return (
 	<div>
-	This is EditExpensePage
+		This is EditExpensePage
+	//This is EditExpensePage with id {props.match.params.id}
 </div>
-);
+  );
+}
 
 const HelpPage = () => (
 	<div>
 	This is HelpPage
 </div>
 );
-
+//a tag used instead of <Link> causes full page refresh when navigated
 const NotFoundPage = () => (
 	<div>
-	404!
+	404! - <Link to=“/”></Link>
 </div>
 );
-
-
-
+const Header = () => (
+	<header>
+	  <h1>Expensify</h1>
+  <NavLink to =“/” activeClassName=”is-active” exact={true}>Home Page</NavLink >
+	  <NavLink to =“/create” activeClassName=”is-active”>Create Page</NavLink >
+  //<NavLink to =“/edit/:id” activeClassName=”is-active”>Edit Page</NavLink >
+  <NavLink to =“/edit” activeClassName=”is-active”>Edit Page</NavLink >
+  <NavLink to =“/help” activeClassName=”is-active”>Help Page</NavLink >
+</header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-<div>
+    <div>
+	<Header />
+<Switch>
   <Route path=”/” component={ExpenseDashboardPage}  exact={true}/>
   <Route path=”/create” component={AddExpensePage} />
   <Route path=”/edit” component={EditExpensePage} />
   <Route path=”/help” component={HelpPage} />
   <Route component={NotFoundPage } />
-</div>
-    </Switch>
+</Switch>
+    </div >
   </BrowserRouter>
 );
 
-ReactDOM.render(routes, document.getElementById(‘test’));
+ReactDOM.render(routes, document.getElementById(“test”));
 
 //webpack to make page route in client side routing
 devServer: {
   historyApiFallback: true;
 }
+
+//scss
+/*.is-active {
+Font-weight: bold;
+}
+
+Create new /router/AppRouter.js
+Move code to the new path
+Modify the import statements as required
+Breakup individual components as well setting up import and exports
+*/
